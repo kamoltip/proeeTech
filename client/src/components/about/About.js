19550f245128321cabcca2e27fba0aaf0c590b79
kamoltip <plawansusu@gmail.com>
2018-02-07 12:44:53 -0500

    MVC && configuration sass--watch && server && ReactRouter

---
+++ client/src/components/about/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+          <div>
+            <div>
+              <h1>I am about</h1>
+            </div>
+          </div>
+    );
+  }
+}
+
+export default About;
---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Home from "./home/Home.js";
+import Dashboard from "./dashboard/Dashboard.js";
+import About from "./about/About.js";
+
+export {
+    Home,
+    Dashboard,
+    About
+}
