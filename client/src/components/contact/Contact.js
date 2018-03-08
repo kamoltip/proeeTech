12c02bd395a1316d55e31cced3c105d4ab08964e
kamoltip <plawansusu@gmail.com>
2018-03-08 01:03:12 -0500

    products animation

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import "./contact.css";
 import {Container, Image} from "semantic-ui-react";
-import {contactBg} from "../../asset/images/geometric3.jpg";
+// import {contactBg} from "../../asset/images/geometric3.jpg";
 import MenuExampleBasic from "../nav/Nav.js";
 import Footer from "../footer/Footer.js";
 
 
 class Contact extends Component {
+
+  getValue = () => {
+    // TODO
+  };
+
+  handleSubmit = () => {
+    // TODO
+
+  };
+
+
+
   render() {
     return (
               <section className="section-contact">
                 <MenuExampleBasic />
                 <Container>
                 <div className="contact">
-                  <image src={contactBg} />
                 </div>
-              </Container>
-              <Footer />
+
+                <button onClick = {this.handleSubmit}>Send Data</button>
+                </Container>
+                <Footer />
               </section>
 
     );
--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import "./products.css";
+import {Container, Image} from "semantic-ui-react";
+// import {lightningFlashDrive} from "../../asset/images/lightningFlashDrive.png";
+import MenuExampleBasic from "../nav/Nav.js";
 
 class Products extends Component {
   render() {
     return (
-          <div>
-            <div>
-              <h1>I am Products</h1>
-            </div>
-          </div>
+
+      <section className="section-products">
+        <MenuExampleBasic />
+        <Container>
+        <div className="products-app"></div>
+        <div className="products-fd"></div>
+
+        </Container>
+      </section>
     );
   }
 }
